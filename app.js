d1becddc07bdc7f7c2eb6bafdf04101d183c8cd8
Jimmy Li <jimmyli921@gmail.com>
2018-01-05 15:33:18 -0800

    Add express validator

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var ejs = require('ejs');
+var expressValidator = require('express-validator');
 
 var app = express();
 
@@ -23,11 +24,51 @@ app.use(bodyParser.urlencoded({extended: false}));
 // Set static path
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+    var namespace = param.split('.')
+    , root        = namespace.shift()
+    , formParam   = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+var users = [
+  {
+    name: "Jimmy",
+    email: "jimmy@test.com",
+    age: 25
+  },
+  {
+    name: "Leia",
+    email: "leia.test.com",
+    age:23
+  }
+];
+
 app.get('/', (req, res) => {
   res.render('index.ejs', {
-    title: "Hello World"
+    title: "Hello World",
+    users
   });
   // res.send("hello");
+});
+
+app.post('/users/add', (req, res) => {
+  const newUser = {
+    name: req.body.name,
+    email: req.body.email,
+    age: req.body.age
+  };
 });
 
 app.listen(3000, () => {
